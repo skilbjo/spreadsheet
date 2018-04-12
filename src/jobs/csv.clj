9f8406bec11b77672a1249163574761efe4dd9ef
John Skilbeck <john.skilbeck@gmail.com>
2018-04-11 20:31:54 -0700

    blah

--- src/jobs/csv.clj
+++ src/jobs/csv.clj
@@ -1,8 +1,25 @@
 (ns jobs.csv
   (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]
+            [clojure.string :as string]
             [windfall.util :as util])
   (:gen-class))
 
+#_(defn- update-data-structure [m v]
+  (if (empty? v)
+    m
+    (recur update-data-structure)))
+
 (defn -main [& args]
-  (log/info "I don't do much... yet"))
+  (log/info "Starting!")
+
+  (->> "input.csv"
+       io/resource
+       slurp
+       (#(clojure.string/split % #"\n"))
+       (map #(clojure.string/split % #","))
+       (map-indexed hash-map)
+       util/print-it)
+
+
+  (log/info "Finished!"))
